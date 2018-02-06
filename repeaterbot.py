a6b5244c74375dc56b333c263aa17ea933620d10
pdurango <silvestri_21@hotmail.com>
2018-02-06 17:14:56 -0500

    Fixed repeaterbot to run on python 2

--- repeaterbot.py
+++ repeaterbot.py
@@ -8,16 +8,16 @@ import time
 import re
 import io
 import sys
-import urllib.request
 import json
 from slackclient import SlackClient
-
+import urllib2
 # instantiate Slack client
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
-weather_api_key = os.environ.get('API_KEY')
+API_KEY = 'ec48c3377c3121185eb753f9ce21fdbf'
+#weather_api_key = os.environ.get('API_KEY')
 
 #list is used for fixing typos
 city_list = [ ]
@@ -52,10 +52,10 @@ def levenshtein(s1, s2):
             substitutions = previous_row[j] + (c1 != c2)
             current_row.append(min(insertions, deletions, substitutions))
         previous_row = current_row
-    
+
     return previous_row[-1]
-    
-#try to correct city based on the city list 
+
+#try to correct city based on the city list
 #returns the closest city by name
 def correct(word):
     min_dist = sys.maxsize
@@ -68,45 +68,52 @@ def correct(word):
 
     return closest_word
 
-    
+
 #rich man's NLP
 #returns tuple(is weather request, city name)
 def NLP(sentence):
     sentence = sentence.lower()
-    
+
     #remove question mark
     if sentence[len(sentence)-1] == '?':
         sentence = sentence[:-1]
-    
+
     words = sentence.split(' ')
 
     #check for a combination of any of these
     tests = [['whats','the','weather','like','in'], ['whats','the','weather','in'], ['hows','the','weather','in'], ['whats','the','weather','for'], ['weather','in'], ['weather','for'], ['weather']]
-    
+
     for test in tests:
         if len(test) >= len(words):
             continue
         total_dist = 0
-        
+
         #test distance from each word
         for i in range(0, len(test)):
             total_dist += levenshtein(words[i], test[i])
-            
-        
+
+
         if total_dist < 6:
             return (True, " ".join(words[len(test):]).title())
-    
+
     return (False, None)
 
-#actually request the data 
+#actually request the data
 #returns string with current conditions
 def request(city):
     if not (city in city_dict):
         city = correct(city)
+    link = 'http://api.openweathermap.org/data/2.5/weather?id=' + str(city_dict[city]) + '&units=metric&APPID=' + API_KEY
+    #print 'reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' + link
+    #with urllib2.Request.urlopen('http://api.openweathermap.org/data/2.5/weather?id=' + str(city_dict[city]) + '&units=metric&appid=' + weather_api_key) as url:
 
-    with urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?id=' + str(city_dict[city]) + '&units=metric&appid=' + weather_api_key) as url:
-        data = json.loads(url.read().decode())
-        return "Current weather in " + city + " is " + data["weather"][0]["description"] + " at " + str(data["main"]["temp"]) + "C"
+        #response = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q=toronto&APPID=ec48c3377c3121185eb753f9ce21fdbf')
+    response = urllib2.urlopen(link)
+
+    data = json.load(response)
+    #print data
+    return "Current weather in " + city + " is " + data["weather"][0]["description"] + " at " + str(data["main"]["temp"]) + "C"
+
 
 #process the message
 def process_message(msg_echo):
@@ -114,21 +121,21 @@ def process_message(msg_echo):
         print('Update found...')
         if 'type' in update and update['type'] == 'message':
             message = parse_direct_mention(update["text"])
-            
+
             (is_req, city_name) = NLP(message)
             #if we're a request, change message to weather
             if is_req:
                 message = request(city_name)
-                
+
             slack_client.rtm_send_message(update["channel"], message)
-    
-            
+
+
 def main():
     #read in city list
     #change to city.list.min.json for all cities
-    with open('canada.list.min.json', mode="r", encoding="utf-8") as f:
+    with open('canada.list.min.json', mode="r") as f:
         json_cities = json.load(f)
-    
+
     #create a dictionary of cities for id, and a list for corrections
     for city in json_cities:
         city_dict[city["name"]] = city["id"]
