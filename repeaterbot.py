a26962393eca51e16faf4f2863b6a6e0dc9b5c75
pdurango <silvestri_21@hotmail.com>
2018-02-05 21:26:20 -0500

    Fixed bot returning @mentioned

--- repeaterbot.py
+++ repeaterbot.py
@@ -14,17 +14,25 @@ slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+def parse_direct_mention(message_text):
+    matches = re.search(MENTION_REGEX, message_text)
+    # the first group contains the username, the second group contains the remaining message
+    return matches.group(2).strip() if matches else None
 
 def main():
     if slack_client.rtm_connect():
         print('RepeaterBot has gone online...')
         while True:
-            print(msg_echo)
             msg_echo = slack_client.rtm_read()
+            print(msg_echo)
             for update in msg_echo:
                 print('Update found...')
                 if 'type' in update and update['type'] == 'message':
-                    slack_client.rtm_send_message(update['channel'], update['text'])
+                    #slack_client.rtm_send_message(update['channel'], update['text'])
+                    message = parse_direct_mention(update["text"])
+                    slack_client.rtm_send_message(update["channel"], message)
             time.sleep(1)
     else:
         print("Connection Failed...")
