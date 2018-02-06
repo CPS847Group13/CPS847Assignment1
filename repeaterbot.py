c917b027793585c48db0800cb56497686976be0c
Lucas <silvestri_21@hotmail.com>
2018-02-05 20:25:07 -0500

    Auto stash before merge of "master" and "origin/master"
    Basic echo bot that repeats messages sent to it

---
+++ repeaterbot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+#export SLACK_BOT_TOKEN=Bot User OAuth Access Token
+
+import os
+import time
+import re
+#import json #used for debug printing
+from slackclient import SlackClient
+
+
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+starterbot_id = None
+
+
+def main():
+    if slack_client.rtm_connect():
+        print('RepeaterBot has gone online...')
+        while True:
+            print(msg_echo)
+            msg_echo = slack_client.rtm_read()
+            for update in msg_echo:
+                print('Update found...')
+                if 'type' in update and update['type'] == 'message':
+                    slack_client.rtm_send_message(update['channel'], update['text'])
+            time.sleep(1)
+    else:
+        print("Connection Failed...")
+
+if __name__ == '__main__':
+    main()
